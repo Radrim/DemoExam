0b3cbc45c7f724a649bf9dbbcba813fcd7aba35e
Радмир Габдрахманов <gabdrahmanov.radmir2018@gmail.com>
2024-02-26 16:59:40 +0300

    add Database connection

--- LopushokApp/LopushokApp/App.xaml.cs
+++ LopushokApp/LopushokApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LopushokApp.ADO;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace LopushokApp
     /// </summary>
     public partial class App : Application
     {
+        public static LopushokBaseEntities Connection = new LopushokBaseEntities();
     }
 }
